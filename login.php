27f81136ec8b3bd479a8ebc4ae5a14681314ce38
Michael Salazar <michael_salazar@gtservice.mx>
2017-02-02 12:07:51 -0800

    Minor bug fixing

--- login.php
+++ login.php
@@ -17,8 +17,8 @@ else
     <meta charset="utf-8">
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <title>GT-Inicio de sesion</title>
-    <link href="css/bootstrap/css/bootstrap.min.css" rel="stylesheet">
     <link rel="stylesheet" href="//maxcdn.bootstrapcdn.com/font-awesome/4.3.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/bootstrap/3.3.7/css/bootstrap.min.css">
     <link rel="stylesheet" href="css/login.css">
     
     
